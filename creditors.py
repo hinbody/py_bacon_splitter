ab352459625ac00eb3b45a5066b7cf2c726112a3
Harley Inbody <hinbody@gmail.com>
2016-02-14 20:23:54 -0500

    adds creditors.py and two function stubs

---
+++ creditors.py
@@ -0,0 +1,9 @@
+# a module to manipulate the creditors table of the PyBaconSplitter db
+# will be using sqlalchemy for the db interaction
+
+def get_all_creditors():
+  pass
+
+def add_creditor_entry():
+  pass
+
